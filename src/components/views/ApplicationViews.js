d4ae36ab856b9c2c18488f3ab698d334ebe1046b
Stacy Lofton <lofton713@gmail.com>
2022-06-20 11:01:17 -0500

    Banner updated with logos-background wallpaper for site

--- src/components/views/ApplicationViews.js
+++ src/components/views/ApplicationViews.js
@@ -8,32 +8,37 @@ import { Home } from "../auth/Home"
 import { MyLots } from "../parking/MyLots"
 import { ParkingEdit } from "../parking/ParkingEdit"
 import { ParkingDetails } from "../parking/ParkingDetails"
+import NSCDarkLogo from '../images/Nashville-SC.png'
 
 export const ApplicationViews = () => {
-	
-       return (
+
+    return (
         <Routes>
             <Route path="/" element={
                 <>
                     <div className="pageHeader">
-                        <h1 className="title">NSC Parking: DEFINED</h1>
-                        <h2 className="motto">Everyone N!!</h2>
+                        <img className='NSCDarkLogo' src={NSCDarkLogo} alt="logo" style={{ width: '200px', height: '100px' }} />
+                        <p>
+                            <h1 className="title">NSC Parking: DEFINED</h1>
+                            <h2 className="motto">Everyone N!!</h2>
+                        </p>
+                        <img className='NSCDarkLogo' src={NSCDarkLogo} alt="logo" style={{ width: '200px', height: '100px' }} />
                     </div>
 
                     <Outlet />
                 </>
             }>
-                
-                <Route path="parkingLots" element={ <ParkingList /> } />
-                <Route path="profile" element={ <Profile /> } />
-                <Route path="supporterGroups" element={ <SupportersList /> } />
-                <Route path="parkingForm" element={ <ParkingForm /> } />
-                <Route path="" element={ <Home /> } />
-                <Route path="myLots" element={ < MyLots /> } />
-                <Route path="parkingEdit/:parkingLotId" element={ < ParkingEdit /> } />
-                <Route path="parkingDetails/:parkingLotId" element={ <ParkingDetails />} />
 
-				
+                <Route path="parkingLots" element={<ParkingList />} />
+                <Route path="profile" element={<Profile />} />
+                <Route path="supporterGroups" element={<SupportersList />} />
+                <Route path="parkingForm" element={<ParkingForm />} />
+                <Route path="" element={<Home />} />
+                <Route path="myLots" element={< MyLots />} />
+                <Route path="parkingEdit/:parkingLotId" element={< ParkingEdit />} />
+                <Route path="parkingDetails/:parkingLotId" element={<ParkingDetails />} />
+
+
             </Route>
         </Routes>
     )
